91abd0de92d9610abe791476dc496936fd99205b
warting <stefan@warting.se>
2022-05-14 16:03:56 +0200

    bump all

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,6 +73,7 @@ android {
 dependencies {
     implementation(project(":lib"))
     implementation("androidx.constraintlayout:constraintlayout:2.1.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.1")
 
     implementation("androidx.core:core-ktx:1.7.0")
     implementation("androidx.appcompat:appcompat:1.4.1")
